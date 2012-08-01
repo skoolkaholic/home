3b792e751bb974b0bafb3d0ba5c1e46e5a79eed4
Aam Agus Mucharam <skoolkaholic@skoolkaholic.local>
2012-08-01 18:53:18 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php 
+
+	mysql_connect("localhost","root","BGQRUR45PK") or die(mysql_error());
+	mysql_select_db("test") or die(mysql_error());
+
+
+	$query = mysql_query("select * from test") or die(mysql_error());
+	while ($row = mysql_fetch_array($query))
+	{
+		echo $row["username"]."<br>";
+	} 
+
+	//added comment 
+?>
+
